dcdcf82e117b5988b1d9eb011268ba3b0edb9267
Josh Nussbaum <joshnuss@gmail.com>
2019-08-14 19:33:19 -0400

    Adds counter logic to app component

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,5 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+function useCounter() {
+  const [count, set] = useState(0)
+
+  return {
+    count,
+    increment: () => set(count + 1),
+    decrement: () => set(count - 1)
+  }
+}
 
 export default function() {
-  return <div>Hello World!</div>
+  const {count, increment, decrement} = useCounter()
+
+  return (
+    <div className="counter">
+      <button className="decrement" onClick={() => decrement()}>-</button>
+      <button className="increment" onClick={() => increment()}>+</button>
+
+      <h2>{count}</h2>
+    </div>
+  )
 }
