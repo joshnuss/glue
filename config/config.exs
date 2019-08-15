1049cdcf75385521e6db76187bbf22d600fc7b50
Josh Nussbaum <joshnuss@gmail.com>
2019-08-14 21:54:13 -0400

    Adds declarative hook definitions

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,13 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+alias PhxHooks.Counter
+
+config :phx_hooks, :hooks, %{
+  counter: %{
+    mod: PhxHooks.Counter,
+    reader: :get,
+    calls: [:increment, :decrement]
+  }
+}
