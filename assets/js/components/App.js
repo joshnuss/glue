a9d4d2fd7e0e0f5bb2f0e1a7700297cc89a79935
Josh Nussbaum <joshnuss@gmail.com>
2019-08-16 03:51:27 -0400

    css fixes

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -9,11 +9,13 @@ export default function() {
   const {count, increment, decrement, update} = useServerState('counter')
 
   return (
-    <div className="counter">
-      <button className="decrement" onClick={() => decrement()}>-</button>
-      <button className="increment" onClick={() => increment()}>+</button>
+    <div className="counter-wrapper">
+      <div className="counter">
+        <button className="decrement" onClick={() => decrement()}>-</button>
+        <button className="increment" onClick={() => increment()}>+</button>
 
-      <h2>{count}</h2>
+        <h2>{count}</h2>
+      </div>
 
       <a onClick={() => update(randomNumber())}>I'm feeling lucky!</a>
     </div>
