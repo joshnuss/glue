e2aa743f8bfed19b638d240cc9393df0bb4a42a3
Josh Nussbaum <joshnuss@gmail.com>
2019-08-16 00:52:18 -0400

    Attempt prod fix

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phx_hooks, PhxHooksWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "react-use-server-state.gigalixirapp.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
