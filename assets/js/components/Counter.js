b5670fd8e79c32d6ced91ea7435e20b4ca81d0e5
Josh Nussbaum <joshnuss@gmail.com>
2019-08-18 04:06:49 -0400

    Updates example to use css grid

--- assets/js/components/Counter.js
+++ assets/js/components/Counter.js
@@ -9,15 +9,12 @@ export default function() {
   const {count, increment, decrement, update} = useServerState('counter')
 
   return (
-    <div className="counter-wrapper">
-      <div className="counter">
-        <button className="decrement" onClick={() => decrement()}>-</button>
-        <button className="increment" onClick={() => increment()}>+</button>
-
-        <h2>{count}</h2>
-      </div>
+    <div className="counter">
+      <button className="decrement" onClick={() => decrement()}>-</button>
+      <h2>{count}</h2>
+      <button className="increment" onClick={() => increment()}>+</button>
 
       <a onClick={() => update(randomNumber())}>I'm feeling lucky!</a>
     </div>
-  )
+ )
 }
