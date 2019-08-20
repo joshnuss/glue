b5f214dee7c75d0ea5c24cd4254ebb8bf9c531bb
Josh Nussbaum <joshnuss@gmail.com>
2019-08-19 20:41:18 -0400

    Removes unused stuff

--- glue/lib/glue.ex
+++ glue/lib/glue.ex
@@ -1,18 +1,2 @@
 defmodule Glue do
-  @moduledoc """
-  Documentation for Glue.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Glue.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
